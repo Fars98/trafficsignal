cc53a78f69e71119dcb5eb474174440ae8f0ae9d
Farseenapm <farseena.p@tataelxsi.co.in>
2023-12-07 15:33:14 +0530

    Seperate thread for traffic signal

--- sig.c
+++ sig.c
@@ -3,11 +3,9 @@
 #include<pthread.h>
 #include<semaphore.h>
 
-sem_t north_south;
-sem_t south_north;
-sem_t east_west;
-sem_t west_east;
+sem_t north_south,south_north,east_west,west_east;
 
+void* signal();
 void* northtosouth();
 void* southtonorth();
 void* easttowest();
@@ -15,22 +13,24 @@ void* westtoeast();
 
 int main()
 {
-        pthread_t t1,t2,t3,t4;
+        pthread_t light,north,south,east,west;
 
         sem_init(&north_south,0,1);
         sem_init(&south_north,0,1);
         sem_init(&east_west,0,1);
         sem_init(&west_east,0,1);
 
-        pthread_create(&t1,NULL,northtosouth,NULL);
-        pthread_create(&t2,NULL,southtonorth,NULL);
-        pthread_create(&t3,NULL,easttowest,NULL);
-        pthread_create(&t4,NULL,westtoeast,NULL);
+	pthread_create(&light,NULL,signal,NULL);
+	pthread_create(&north,NULL,northtosouth,NULL);
+        pthread_create(&south,NULL,southtonorth,NULL);
+        pthread_create(&east,NULL,easttowest,NULL);
+        pthread_create(&west,NULL,westtoeast,NULL);
 
-        pthread_join(t1,NULL);
-        pthread_join(t2,NULL);
-        pthread_join(t3,NULL);
-        pthread_join(t4,NULL);
+	pthread_join(light,NULL);
+        pthread_join(north,NULL);
+        pthread_join(south,NULL);
+        pthread_join(east,NULL);
+        pthread_join(west,NULL);
 
         sem_destroy(&north_south);
         sem_destroy(&south_north);
@@ -41,79 +41,59 @@ int main()
 }
 
 
-void* northtosouth()
+void* signal()
 {
-        printf("Vehicles waiting for north to south\n");
         sem_wait(&east_west);
-        printf("East to west clear\n");
         sem_wait(&west_east);
-        printf("West to east clear\n");
-        sem_wait(&north_south);
-        printf("North to south clear\n");
-        printf("Vehicles moving north to south\n");
+
+        printf("Green in north-south direction\n");
+
+        sem_post(&south_north);
+        sem_post(&north_south);
+
         sleep(5);
 
-        sem_post(&north_south);
-        sem_post(&west_east);
-        sem_post(&east_west);
+        sem_wait(&north_south);
+        sem_wait(&south_north);
 
-        printf("North to south vehicles finished moving\n\n");
+        printf("Green in east-west direction\n");
+
+        sem_post(&east_west);
+        sem_post(&west_east);
+
+        sleep(5);
+}
+
+
+void* northtosouth()
+{
+        sleep(1);
+        sem_wait(&north_south);
+        printf("Vehicles moving in north-south direction\n");
+        sem_post(&north_south);
 }
 
 void* southtonorth()
 {
-        printf("Vehicles waiting for south to north\n");
-        sem_wait(&east_west);
-        printf("East to west clear\n");
-        sem_wait(&west_east);
-        printf("West to east clear\n");
+        sleep(1);
         sem_wait(&south_north);
-        printf("South to north clear\n");
-        printf("Vehicles moving south to north\n");
-        sleep(5);
-
+        printf("Vehicles moving in south-north direction\n");
         sem_post(&south_north);
-        sem_post(&west_east);
-        sem_post(&east_west);
-
-        printf("South to north vehicles finished moving\n\n");
-
 }
+
 void* easttowest()
 {
-        printf("Vehicles waiting for east to west\n");
+        sleep(1);
         sem_wait(&east_west);
-        printf("East to west clear\n");
-        sem_wait(&north_south);
-        printf("North to south clear\n");
-        sem_wait(&south_north);
-        printf("South to north clear\n");
-
-        printf("Vehicles moving east to west\n");
-        sleep(5);
-
-        sem_post(&south_north);
-        sem_post(&north_south);
+        printf("Vehicles moving in east-west direction\n");
         sem_post(&east_west);
-
-        printf("East to west vehicles finished moving\n\n");
-
 }
+
 void* westtoeast()
 {
-        printf("Vehicles waiting for west to east\n");
+        sleep(1);
         sem_wait(&west_east);
-        printf("West to east clear\n");
-        sem_wait(&north_south);
-        printf("North to south clear\n");
-	sem_wait(&south_north);
-        printf("South to north clear\n");
-	printf("Vehicles moving west to east\n");
-        sleep(5);
-	sem_post(&south_north);
-        sem_post(&north_south);
+        printf("Vehicles moving in west-east direction\n");
         sem_post(&west_east);
-	printf("West to east Vehicles finished moving\n\n");
-
 }
 
